775bdffa62483ed3f50831aff034d70fae479db3
David Teixeira de Masin <davidteixeira.info@gmail.com>
2025-03-15 17:48:09 -0300

    Cenário com ênfase no teste consentimento de cookies

---
+++ cypress/e2e/cookies.cy.js
@@ -0,0 +1,33 @@
+/// <reference types="cypress"/>
+
+describe("Cypress Simulator - Cookies Consent", () => {
+  beforeEach(() => {
+    cy.visit("./src/index.html?skipCaptcha=true");
+    cy.contains("button", "Login").click();
+  });
+  it("it consents on the cookies usage", () => {
+    cy.get("#cookieConsent")
+      .as("cookieConsentBanner")
+      .find("button:contains('Accept')")
+      .click();
+
+    cy.get("@cookieConsentBanner").should("not.be.visible");
+
+    cy.window()
+      .its("localStorage.cookieConsent")
+      .should("be.equal", "accepted");
+  });
+
+  it("it declines on the cookies usage", () => {
+    cy.get("#cookieConsent")
+      .as("cookieConsentBanner")
+      .find("button:contains('Decline')")
+      .click();
+
+    cy.get("@cookieConsentBanner").should("not.be.visible");
+
+    cy.window()
+      .its("localStorage.cookieConsent")
+      .should("be.equal", "declined");
+  });
+});
