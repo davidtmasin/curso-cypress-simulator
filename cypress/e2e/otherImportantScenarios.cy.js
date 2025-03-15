3247e7fd400d79e8408b28e2f6c50bcd611023ae
David Teixeira de Masin <davidteixeira.info@gmail.com>
2025-03-15 17:51:45 -0300

    Separação dos cenários de teste: Smoke Test e Demais Cenários Importantes

---
+++ cypress/e2e/otherImportantScenarios.cy.js
@@ -0,0 +1,59 @@
+describe('template spec', () => {
+  //A partir deste próximo caso de teste, daremos início àqueles não-essenciais porém, necessários.
+  it("it logs out successfully", () => {
+    cy.get("#sandwich-menu").click();
+
+    cy.contains("button", "Logout").should("be.visible").click();
+
+    cy.contains("h2", "Let's get started!").should("be.visible");
+    cy.contains("button", "Login").should("be.visible");
+    cy.get("#sandwich-menu").should("not.be.visible");
+  });
+
+  it("it shows and hides the logout button", () => {
+    cy.get("#sandwich-menu").click();
+
+    cy.contains("button", "Logout").should("be.visible");
+
+    cy.get("#sandwich-menu").click();
+
+    cy.contains("button", "Logout").should("not.be.visible");
+  });
+
+  it("it shows the running state before showing the final result", () => {
+    cy.contains("button", "Run").should("have.attr", "disabled");
+
+    cy.get("#codeInput").type("cy.visit()");
+
+    cy.contains("button", "Run").should("not.have.attr", "disabled");
+
+    cy.contains("button", "Run").click();
+
+    cy.contains("button", "Running...")
+      .should("be.visible")
+      .and("have.attr", "disabled");
+    cy.contains("#outputArea", "Running... Please wait.").should("be.visible");
+    cy.contains("button", "Running...", { timeout: 6000 }).should("not.exist");
+    cy.contains("button", "Run").should("be.visible");
+    cy.contains("#outputArea", "Running... Please wait.", {
+      timeout: 6000,
+    }).should("not.exist");
+    cy.get("#outputArea")
+      .should("contain", "Success")
+      .and("contain", "cy.visit() // Visited URL");
+  });
+  
+  it("captcha button states", () => {});
+  
+  it("captcha error", () => {});
+  
+  it("Run button - enabled/disabled states", () => {});
+
+  it("reset textarea on logout and login", () => {});
+
+  it("disabled run button on logout and login", () => {});
+
+  it("reset  output on logout and login", () => {});
+
+  it("no cooking banner on the login page", () => {});
+})
--- cypress/e2e/smokeTest.cy.js
+++ cypress/e2e/smokeTest.cy.js
@@ -1,6 +1,6 @@
 /// <reference types="cypress"/>
 
-describe("Cypress Simulator", () => {
+describe("Cypress Simulator - Smoke Test", () => {
   beforeEach(() => {
     cy.visit("./src/index.html?skipCaptcha=true", {
       onBeforeLoad(windowBrowser) {
@@ -112,66 +112,4 @@ describe("Cypress Simulator", () => {
     cy.get("#expandIcon").should("be.visible");
   });
   // Até este último caso de teste acima, temos uma ótima cobertura para um possível Smoke Test.
-
-  //A partir deste próximo caso de teste, daremos início àqueles não-essenciais porém, necessários.
-  it("it logs out successfully", () => {
-    cy.get("#sandwich-menu").click();
-
-    cy.contains("button", "Logout").should("be.visible").click();
-
-    cy.contains("h2", "Let's get started!").should("be.visible");
-    cy.contains("button", "Login").should("be.visible");
-    cy.get("#sandwich-menu").should("not.be.visible");
-  });
-
-  it("it shows and hides the logout button", () => {
-    cy.get("#sandwich-menu").click();
-
-    cy.contains("button", "Logout").should("be.visible");
-
-    cy.get("#sandwich-menu").click();
-
-    cy.contains("button", "Logout").should("not.be.visible");
-  });
-
-  it("it shows the running state before showing the final result", () => {
-    cy.contains("button", "Run").should("have.attr", "disabled");
-
-    cy.get("#codeInput").type("cy.visit()");
-
-    cy.contains("button", "Run").should("not.have.attr", "disabled");
-
-    cy.contains("button", "Run").click();
-
-    cy.contains("button", "Running...")
-      .should("be.visible")
-      .and("have.attr", "disabled");
-    cy.contains("#outputArea", "Running... Please wait.").should("be.visible");
-    cy.contains("button", "Running...", { timeout: 6000 }).should("not.exist");
-    cy.contains("button", "Run").should("be.visible");
-    cy.contains("#outputArea", "Running... Please wait.", {
-      timeout: 6000,
-    }).should("not.exist");
-    cy.get("#outputArea")
-      .should("contain", "Success")
-      .and("contain", "cy.visit() // Visited URL");
-  });
-
-  it("Run button - enabled/disabled states", () => {});
-
-  it("accepts cookies", () => {});
-
-  it("decline cookies", () => {});
-
-  it("captcha button states", () => {});
-
-  it("captcha error", () => {});
-
-  it("reset textarea on logout and login", () => {});
-
-  it("disabled run button on logout and login", () => {});
-
-  it("reset  output on logout and login", () => {});
-
-  it("no cooking banner on the login page", () => {});
 });
