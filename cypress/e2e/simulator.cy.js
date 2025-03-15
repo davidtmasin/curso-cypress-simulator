1f349f5ae3420ea9dbe485377b08364ef6ea3b2c
David Teixeira de Masin <davidteixeira.info@gmail.com>
2025-03-15 16:21:18 -0300

    Maximiza e Minimiza o resultado da simulação e Faz logout com sucesso

--- cypress/e2e/simulator.cy.js
+++ cypress/e2e/simulator.cy.js
@@ -58,7 +58,7 @@ describe("Cypress Simulator", () => {
       .and("be.visible");
   });
 
-  it.only("it asks for help and gets common Cypress commands and examples with a link to the docs", () => {
+  it("it asks for help and gets common Cypress commands and examples with a link to the docs", () => {
     cy.get("textarea[placeholder='Write your Cypress code here...']").type(
       "help"
     );
@@ -83,17 +83,66 @@ describe("Cypress Simulator", () => {
       .and("be.visible");
   });
 
-  it("maximize/minimize", () => {});
-  it("logout", () => {});
+  it("it maximizes and minimizes a simulation result", () => {
+    cy.get("textarea[placeholder='Write your Cypress code here...']").type(
+      "help"
+    );
+    cy.contains("button", "Run").click();
+
+    cy.get(".expand-collapse")
+      .as("btnExpandCollapse")
+      .should("have.attr", "aria-expanded", "false");
+    cy.get("#expandIcon").should("be.visible");
+
+    cy.get("@btnExpandCollapse").click();
+
+    cy.get("#outputArea", { timeout: 6000 })
+      .should("contain", "Common Cypress commands and examples:")
+      .and(
+        "contain",
+        "For more commands and details, visit the official Cypress API documentation."
+      )
+      .and("be.visible");
+    cy.get("@btnExpandCollapse").should("have.attr", "aria-expanded", "true");
+    cy.get("#collapseIcon").should("be.visible");
+
+    cy.get("@btnExpandCollapse").click();
+
+    cy.get("@btnExpandCollapse").should("have.attr", "aria-expanded", "false");
+    cy.get("#expandIcon").should("be.visible");
+  });
+  // Até este último caso de teste acima, temos uma ótima cobertura para um possível Smoke Test.
+
+  //A partir deste próximo caso de teste, daremos início àqueles não-essenciais porém, necessários.
+  it("it logs out successfully", () => {
+    cy.get("#sandwich-menu").click();
+
+    cy.contains("button", "Logout").should("be.visible").click();
+
+    cy.contains("h2", "Let's get started!").should("be.visible");
+    cy.contains("button", "Login").should("be.visible");
+    cy.get("#sandwich-menu").should("not.be.visible");
+  });
+
   it("show and hide logout button", () => {});
+
   it("Running... state", () => {});
+
   it("Run button - enabled/disabled states", () => {});
+
   it("accepts cookies", () => {});
+
   it("decline cookies", () => {});
+
   it("captcha button states", () => {});
+
   it("captcha error", () => {});
+
   it("reset textarea on logout and login", () => {});
+
   it("disabled run button on logout and login", () => {});
+
   it("reset  output on logout and login", () => {});
+
   it("no cooking banner on the login page", () => {});
 });
